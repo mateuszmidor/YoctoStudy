a554edec84236441d9713259aabd7e322537ecc9
Mateusz Midor <mateusz>
2016-07-12 10:08:58 +0200

    INIT

--- poky/meta/classes/nopackages.bbclass
+++
@@ -1,6 +0,0 @@
-deltask do_package
-deltask do_package_write_rpm
-deltask do_package_write_ipk
-deltask do_package_write_deb
-deltask do_package_qa
-deltask do_packagedata
---
+++ source/main.cpp
@@ -0,0 +1,14 @@
+/**
+ *   @file: main.cpp
+ *
+ *   @date: Jul 12, 2016
+ * @author: Mateusz Midor
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "CustomRecipeDemo (built under yocto) was born!" << endl;
+    return 0;
+}
